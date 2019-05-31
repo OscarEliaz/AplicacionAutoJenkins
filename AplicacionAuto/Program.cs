a011f9edc8ac98865443544dcc65b6e036c6bc80
WTO Eliaz <oscar__x@hotmail.com>
2019-05-31 13:01:22 -0400

    Update Program.cs
    ahora

--- AplicacionAuto/Program.cs
+++ AplicacionAuto/Program.cs
@@ -6,7 +6,7 @@ namespace AplicacionAuto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");ghfhh
         }
     }
 }
