20a72da0eac2b24a3b388ce44cb7a01492e7a077
WTO Eliaz <oscar__x@hotmail.com>
2019-05-31 14:46:23 -0400

    Update Program.cs
    falla

--- AplicacionAuto/Program.cs
+++ AplicacionAuto/Program.cs
@@ -6,7 +6,11 @@ namespace AplicacionAuto
     {
         static void Main(string[] args)
         {
+<<<<<<< HEAD
             Console.WriteLine("Hello World!");jifildfilf
+=======
+            Console.WriteLine("Hello World!");ghfhh
+>>>>>>> parent of 9a0ae31... Update Program.cs
         }
     }
 }
