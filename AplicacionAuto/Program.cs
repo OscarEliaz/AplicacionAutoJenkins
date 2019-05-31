429b242b85d62672c411b79300434318a7f549fa
WTO Eliaz <oscar__x@hotmail.com>
2019-05-31 17:57:41 -0400

    Update Program.cs
    Añadir

--- AplicacionAuto/Program.cs
+++ AplicacionAuto/Program.cs
@@ -6,7 +6,20 @@ namespace AplicacionAuto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int _ruedas=4;
+    
+            Muestra();
+            Pausa();
+        }
+        public static void Muestra()
+        {
+            Console.WriteLine("Hola el auto tiene");
+        }
+
+        public static void Pausa()
+        {
+            Console.WriteLine("Presione cualquier tecla para continuar ...");
+            Console.ReadKey();
         }
     }
 }
