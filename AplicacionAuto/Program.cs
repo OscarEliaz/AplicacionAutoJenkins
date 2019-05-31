f4ca0c7463db40f5009a5719dbca1e0f9b0ca6e4
WTO Eliaz <oscar__x@hotmail.com>
2019-05-31 12:44:35 -0400

    Agregar archivos de proyecto.

---
+++ AplicacionAuto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AplicacionAuto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
