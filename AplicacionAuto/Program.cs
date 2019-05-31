62876b61e83abc4c4a7b42a5363b1be57652dcd7
WTO Eliaz <oscar__x@hotmail.com>
2019-05-31 14:48:50 -0400

    Revert "Update Program.cs"
    This reverts commit 0eb915ee829155b7efec5b87c287aabfbd15653e.

--- AplicacionAuto/Program.cs
+++ AplicacionAuto/Program.cs
@@ -6,7 +6,11 @@ namespace AplicacionAuto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+<<<<<<< HEAD
+            Console.WriteLine("Hello World!");jifildfilf
+=======
+            Console.WriteLine("Hello World!");ghfhh
+>>>>>>> parent of 9a0ae31... Update Program.cs
         }
     }
 }
