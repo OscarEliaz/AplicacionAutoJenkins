9a0ae319741a3c2459fe55966653d92021f3ff37
WTO Eliaz <oscar__x@hotmail.com>
2019-05-31 14:10:01 -0400

    Update Program.cs
    arreglo

--- AplicacionAuto/Program.cs
+++ AplicacionAuto/Program.cs
@@ -6,7 +6,7 @@ namespace AplicacionAuto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");ghfhh
+            Console.WriteLine("Hello World!");
         }
     }
 }
